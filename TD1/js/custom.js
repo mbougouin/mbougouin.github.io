ced3de42bcb90dd7e5baf211476ac96ff7b050c4
Morgane Bougouin <mbougouin35@gmail.com>
2017-04-23 19:21:05 +0200

    fix cors error with redirect http to https

--- TD1/js/custom.js
+++ TD1/js/custom.js
@@ -3,15 +3,15 @@
 
     function requestApi(url, elementId) {
         $.get(url, function (response) {
-            $("#id").html(response);
+            $("#" + elementId).html(JSON.stringify(response), null, 2);
         });
     }
 
     function homePageLoading() {
-        requestApi('http://bitcoin.mubiz.com/info', 'info');
-        requestApi('http://bitcoin.mubiz.com/blockchaininfo', 'blockchaininfo');
-        requestApi('http://bitcoin.mubiz.com/mininginfo', 'mininginfo');
-        requestApi('http://bitcoin.mubiz.com/peerinfo', 'peerinfo');    
+        requestApi('https://bitcoin.mubiz.com/info', 'info');
+        requestApi('https://bitcoin.mubiz.com/blockchaininfo', 'blockchaininfo');
+        requestApi('https://bitcoin.mubiz.com/mininginfo', 'mininginfo');
+        requestApi('https://bitcoin.mubiz.com/peerinfo', 'peerinfo');    
     }
 
     $(document).ready(homePageLoading);
